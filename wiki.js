3e3af0758fc313f0e9c98149131e5b61ba20cc39
monty13kathare <arvindkathare8182@gmail.com>
2026-05-11 17:42:53 +0530

    Fix: Add startup error handling and port mapping for Render

--- wiki.js
+++ wiki.js
@@ -1,1 +1,8 @@
-require('./server')
+try {
+  console.log('Starting Wiki.js...')
+  require('./server')
+} catch (err) {
+  console.error('CRITICAL ERROR DURING STARTUP:')
+  console.error(err)
+  process.exit(1)
+}
